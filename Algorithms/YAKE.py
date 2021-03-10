9280f9d374de4ea28d6eecbf5eb5b77ffe644944
Maurice Gerber <maurice.gerber2@gmail.com>
2021-03-10 18:55:34 +0100

    Update YAKE.py
    update

--- Algorithms/YAKE.py
+++ Algorithms/YAKE.py
@@ -11,7 +11,7 @@ text = str(df)
 language = "en"
 max_ngram_size = 2 # length of Keywords
 deduplication_threshold = 0.1 # limit the duplication of words in different keywords (0.9 = allowed, 0.1 = avoid)
-numOfKeywords = 50 # number of keywords
+numOfKeywords = 3 # number of keywords
 custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, 
 	dedupLim=deduplication_threshold, top=numOfKeywords, features=None)
 keywords = custom_kw_extractor.extract_keywords(text)
