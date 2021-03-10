e4adfa80747ca0e4c5d3f3eaf8425d46afb5d60b
Maurice Gerber <maurice.gerber2@gmail.com>
2021-03-10 17:34:58 +0100

    test
    update for test

--- Algorithms/YAKE.py
+++ Algorithms/YAKE.py
@@ -2,7 +2,7 @@ import pandas as pd
 import yake
 from yake import KeywordExtractor
 
-df = pd.read_csv("./us_election_2020_1st_presidential_debate.csv")
+df = pd.read_csv("./Test_Text.csv")
 
 kw_extractor = yake.KeywordExtractor()
 text = str(df)
