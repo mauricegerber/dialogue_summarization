2ff38ea68312a433ea4a42a323065da43e814fc2
Maurice Gerber <maurice.gerber2@gmail.com>
2021-03-10 17:31:26 +0100

    Update YAKE.py
    update

---
+++ Algorithms/YAKE.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import yake
+from yake import KeywordExtractor
+
+df = pd.read_csv("./Data/us_election_2020_1st_presidential_debate.csv")
+
+kw_extractor = yake.KeywordExtractor()
+text = str(df)
