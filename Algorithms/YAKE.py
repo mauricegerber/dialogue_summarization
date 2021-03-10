5e09d06d9e787087a2ca45a130600196b1f8973a
Maurice Gerber <maurice.gerber2@gmail.com>
2021-03-10 17:33:41 +0100

    Update YAKE.py
    update

--- Algorithms/YAKE.py
+++ Algorithms/YAKE.py
@@ -6,3 +6,11 @@ df = pd.read_csv("./us_election_2020_1st_presidential_debate.csv")
 
 kw_extractor = yake.KeywordExtractor()
 text = str(df)
+language = "en"
+max_ngram_size = 2 # length of Keywords
+deduplication_threshold = 0.1 # limit the duplication of words in different keywords (0.9 = allowed, 0.1 = avoid)
+numOfKeywords = 10 # number of keywords
+custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None)
+keywords = custom_kw_extractor.extract_keywords(text)
+for kw in keywords:
+	print(kw)
