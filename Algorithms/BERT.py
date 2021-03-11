1eee272513574c27670f5e761a80c80101bb0270
basilrohr <basilrohr@users.noreply.github.com>
2021-03-11 13:53:30 +0100

    update BERT (now running)

--- Algorithms/BERT.py
+++ Algorithms/BERT.py
@@ -1,14 +1,20 @@
-try:
-    %tensorflow_version 2.x
-except Exception:
-    pass
-import tensorflow as tf
-
-import tensorflow_hub as hub
-
-from tensorflow.keras import layers
-import bert
 import pandas as pd
 
+from keybert import KeyBERT
 
-pd.read_csv("./Test_Text.csv")
+
+#transcript = pd.read_csv("C:\\Users\\basil\\myCloud\\01 Studium\\ZHAW\\Bachelor Wirtschaftsingenieurwesen\\6. Semester\\BA\\summarization\\Dash\\us_election_2020_vice_presidential_debate.csv")
+
+transcript = pd.read_csv("C:\\Users\\basil\\Documents\\summarization\\summarization\\Dash\\us_election_2020_vice_presidential_debate.csv")
+kw_extractor = KeyBERT('distilbert-base-nli-mean-tokens')
+
+kw_extractor = KeyBERT('distilbert-base-nli-mean-tokens')
+for j in range(len(transcript)):
+    try:
+        keywords = kw_extractor.extract_keywords(transcript["text"][j], keyphrase_ngram_range=(1,3), stop_words = "english", diversity=0.7, use_mmr=True)
+        # Keyprhase range = min/max number of words
+        # For using diversity parameter, use_mmr=True is needed
+        print(keywords)
+    except:
+        print("empty")
+
--- Dash/extract_keywords_test.py
+++ Dash/extract_keywords_test.py
@@ -1,16 +1,13 @@
 import pandas as pd
 
-from summa import keywords
-from pytopicrank import TopicRank
-
 from keybert import KeyBERT
 
 
 
 
-transcript = pd.read_csv("C:\\Users\\pasca\\myCloud\\01 Studium\\ZHAW\\Bachelor Wirtschaftsingenieurwesen\\6. Semester\\BA\\summarization\\Dash\\us_election_2020_vice_presidential_debate.csv")
+#transcript = pd.read_csv("C:\\Users\\basil\\myCloud\\01 Studium\\ZHAW\\Bachelor Wirtschaftsingenieurwesen\\6. Semester\\BA\\summarization\\Dash\\us_election_2020_vice_presidential_debate.csv")
 
-
+transcript = pd.read_csv("C:\\Users\\basil\\Documents\\summarization\\summarization\\Dash\\us_election_2020_vice_presidential_debate.csv")
 kw_extractor = KeyBERT('distilbert-base-nli-mean-tokens')
 for j in range(len(transcript)):
     keywords = kw_extractor.extract_keywords(transcript["text"][j], keyphrase_length=1, stop_words='english')
