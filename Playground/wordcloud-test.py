7dab34aacb0581c858ad909459f847f365606a26
basilrohr <basilrohr@users.noreply.github.com>
2021-04-15 13:48:36 +0200

    wordcloud each 5 min of Discussion

--- Playground/wordcloud-test.py
+++ Playground/wordcloud-test.py
@@ -3,6 +3,7 @@ import pandas as pd
 from nltk.corpus import stopwords, wordnet
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import datetime
 
 transcript = pd.read_csv(
     filepath_or_buffer="Playground\Vice presidential debate.csv",
@@ -18,6 +19,7 @@ print()
 transcript["Time"] = transcript["Time"].str.replace("60", "59")
 
 stop_words = set(stopwords.words("english"))
+stop_words.append("Thank", "Joe", "Biden", "Donald", "Trump", "America", "American")
 
 
 text = ""
@@ -29,12 +31,35 @@ for t in transcript["Utterance"]:
         text += " " + t + "\n\n"
         counter = 0
 
-print(transcript["Time"])
+
+ti = transcript.iloc[72, 1]
+min = int(ti[0:2])
+
+steps = 5
+minutes = range(0, min, steps)
+minutes = minutes[1:len(minutes)]
+
+counter = 0
+index_min = 0
+for t in transcript["Utterance"]:
+    if counter < minutes[index_min]:
+        counter += 1
+        text += " " + t
+    else:
+        index_min += 1
+        wordcloud = WordCloud(stopwords=stop_words).generate(text)
+        plt.figure(figsize=(15,10))
+        plt.clf()
+        plt.imshow(wordcloud)
+        plt.show()
 
 
-wordcloud = WordCloud(stopwords=stop_words).generate(text)
 
-plt.figure(figsize=(15,10))
-plt.clf()
-plt.imshow(wordcloud)
-plt.show()
+
+
+# wordcloud = WordCloud(stopwords=stop_words).generate(text)
+
+# plt.figure(figsize=(15,10))
+# plt.clf()
+# plt.imshow(wordcloud)
+# plt.show()
