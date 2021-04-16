186a81c233fcd7697ec354e1f8512cd0dcbfa8be
basilrohr <basilrohr@users.noreply.github.com>
2021-04-16 11:42:19 +0200

    update wordcloud per section (gives now separate wordcloud images

--- Playground/wordcloud-test.py
+++ Playground/wordcloud-test.py
@@ -13,48 +13,59 @@ transcript = pd.read_csv(
 )
 
 
-print(transcript.head(5))
-print()
-
 transcript["Time"] = transcript["Time"].str.replace("60", "59")
 
 stop_words = set(stopwords.words("english"))
-stop_words.append("Thank", "Joe", "Biden", "Donald", "Trump", "America", "American")
+stop_words |= set(["Thank", "Joe", "Biden", "Donald", "Trump", "America", "American", "President"])
+
+
+nrow = len(transcript.index)-1
+
+last_min = transcript.iloc[nrow, 1]
+l_min = int(last_min[0:2])
+
+steps = 5
+minutes = range(0, l_min+steps, steps)
+minutes = minutes[1:]
+
+
+def analyse_minute(timestamp):
+    minute = 0
+    if len(timestamp) > 5:
+        minute = 60 * int(timestamp[0:timestamp.find(":",0,len(timestamp))])
+        timestamp = timestamp[timestamp.find(":",0,len(timestamp))+1:]
+        
+    minute += int(timestamp[0:timestamp.find(":",0,len(timestamp))])
+    return(minute)
 
 
 text = ""
-counter = 0
-for t in transcript["Utterance"]:
-    counter += 1
-    text += " " + t
-    if counter == 10:
-        text += " " + t + "\n\n"
-        counter = 0
-
-
-ti = transcript.iloc[72, 1]
-min = int(ti[0:2])
-
-steps = 5
-minutes = range(0, min, steps)
-minutes = minutes[1:len(minutes)]
-
-counter = 0
 index_min = 0
-for i in range(min):
-    t = transcript[i,5]
-
-for t in transcript["Utterance"]:
-    if counter < minutes[index_min]:
-        counter += 1
+for i in range(nrow+1):
+    t = transcript.iloc[i]["Utterance"]
+    min_count = analyse_minute(transcript.iloc[i]["Time"])
+    
+    if min_count < minutes[index_min]:
         text += " " + t
+      
+
     else:
         index_min += 1
+        
         wordcloud = WordCloud(stopwords=stop_words).generate(text)
         plt.figure(figsize=(15,10))
         plt.clf()
         plt.imshow(wordcloud)
         plt.show()
+        text = transcript.iloc[i]["Utterance"]
+
+    if i == nrow:
+        wordcloud = WordCloud(stopwords=stop_words).generate(text)
+        plt.figure(figsize=(15,10))
+        plt.clf()
+        plt.imshow(wordcloud)
+        plt.show()
+
 
 
 
