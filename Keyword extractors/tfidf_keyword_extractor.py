1c3a70aedd5de49d880d91c8d536a938a6486416
Pascal Aigner <pascal.aigner97@gmail.com>
2021-05-27 14:53:44 +0200

    added textrank and tf-idf

--- Keyword extractors/tfidf_keyword_extractor.py
+++ Keyword extractors/tfidf_keyword_extractor.py
@@ -1,12 +1,13 @@
 import sys
 import os
-
-# https://github.com/summanlp/textrank
-from summa import keywords
+import pandas as pd
 
 # add absolute path to main directory "summarization" to system paths
 sys.path.insert(0, os.path.split(sys.path[0])[0])
 path = sys.path[0]
+
+# https://en.wikipedia.org/wiki/Tf%E2%80%93idf
+from Dash.functions.tfidf import tfidf
 
 # read sample file and create list of sentences
 f = open(path + "/Keyword extractors/bbc_sample_text.txt", "r")
@@ -15,6 +16,6 @@ for line in f:
     sentences.append(line.strip("\n"))
 # print(sentences)
 
-for sentence in sentences:
-    kws = keywords.keywords(sentence, words=5).split("\n")
-    print(kws)
+pd.set_option("display.max_rows", 1000)
+df = tfidf(sentences)
+print(df)
